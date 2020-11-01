c304f9ea22e85ce01138f9958448e200a72f9bb6
Alice Cecile <alice.i.cecile@gmail.com>
2020-11-01 18:01:07 -0500

    Signal structs for organization

--- asm-lib/src/utils.rs
+++ asm-lib/src/utils.rs
@@ -3,3 +3,17 @@ pub struct Position {
 	pub x: isize,
 	pub y: isize
 }
+
+pub enum ID {
+	Empty,
+	Hive,
+	Plant,
+	Fungus
+}
+
+pub enum SignalType {
+	Passive (ID),
+	Push (ID),
+	Pull (ID),
+	Work
+}
