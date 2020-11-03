f27f62f61a181360b751b48a9fcf2ea26b0258a0
Alice Cecile <alice.i.cecile@gmail.com>
2020-11-03 01:00:05 -0500

    Split out graphics config and scene drawing

--- asm-lib/src/graphics.rs
+++ asm-lib/src/graphics.rs
@@ -6,20 +6,35 @@ pub struct GraphicsPlugin;
 impl Plugin for GraphicsPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app
-        .add_system(hello_graphics.system());
+        .add_startup_system(setup_graphics.system())
+        .add_system(render.system());
+
     }
 }
 
-fn hello_graphics(
+fn setup_graphics(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>
+) {
+    let _materials =  asset_server.load_folder("");
+
+    commands
+        .spawn(Camera2dComponents::default());
+}
+
+fn render(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let texture_handle = asset_server.load("clover.png");
+    mut materials: ResMut<Assets<ColorMaterial>>
+){
+    let clover = asset_server.get_handle("clover.png");
+
     commands
-        .spawn(Camera2dComponents::default())
-        .spawn(SpriteComponents {
-            material: materials.add(texture_handle.into()),
-            ..Default::default()
-        });
+    .spawn(SpriteComponents {
+        // TODO: This seems incredibly unidiomatic and doesn't match the AssetServer example
+        material: materials.add(clover.into()),
+        transform: Transform::from_translation(Vec3::new(0.0, 50.0, 1.0)),
+        sprite: Sprite::new(Vec2::new(500.0, 500.0)),
+        ..Default::default()
+    });
 }
