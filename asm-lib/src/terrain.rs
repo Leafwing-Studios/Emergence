c10e427734d55e3494878e2ddc91623ee95d95ac
Alice Cecile <alice.i.cecile@gmail.com>
2020-11-03 21:55:23 -0500

    Removed unneeded debugging system

--- asm-lib/src/terrain.rs
+++ asm-lib/src/terrain.rs
@@ -8,10 +8,6 @@ pub struct Tile {}
 pub struct TerrainPlugin;
 impl Plugin for TerrainPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_system(render_terrain.system());
+        app;
     }
-}
-
-fn render_terrain(_tile: &Tile, position: &Position) {
-    println!("Tile: ({}, {})", position.x, position.y);
 }
