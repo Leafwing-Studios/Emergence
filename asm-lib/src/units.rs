878b5efc896594df24351d8d754c0e7614d1f874
Alice Cecile <alice.i.cecile@gmail.com>
2020-11-04 17:15:00 -0500

    Ants wander randomly

--- asm-lib/src/units.rs
+++ asm-lib/src/units.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
+use rand::distributions::Standard;
+use rand::Rng;
 
-use crate::utils::Position;
+use crate::utils::{HexDirection, Position};
 
 pub struct Unit {}
 pub struct Ant {}
@@ -8,7 +10,7 @@ pub struct Ant {}
 pub struct UnitsPlugin;
 impl Plugin for UnitsPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_resource(UnitTimer(Timer::from_seconds(2.0, true)))
+        app.add_resource(UnitTimer(Timer::from_seconds(0.5, true)))
             .add_system(plan.system())
             .add_system(act.system())
             .add_system(maintain_units.system());
@@ -28,9 +30,18 @@ fn act(
     timer.0.tick(time.delta_seconds);
     if timer.0.finished {
         for (_, mut position) in query.iter_mut() {
-            position.x = position.x + 1;
+            let mut rng = &mut rand::thread_rng();
+            let direction: HexDirection = rng.sample(Standard);
+
+            let offset = direction.offset();
+
+            position.alpha += offset.alpha;
+            position.beta += offset.beta;
         }
     }
 }
+
+// NE <-> SE
+// NW <-> SW
 
 fn maintain_units(mut commands: Commands) {}
