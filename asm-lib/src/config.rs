6d09ad86d90e453e4a0a411e676c76795c7698f7
Alice Cecile <alice.i.cecile@gmail.com>
2020-11-03 21:54:59 -0500

    Set better defaults for map size

--- asm-lib/src/config.rs
+++ asm-lib/src/config.rs
@@ -1,11 +1,11 @@
 pub const WINDOW_WIDTH: u32 = 1920;
 pub const WINDOW_HEIGHT: u32 = 1080;
-pub const TILE_SIZE: u32 = 50;
+pub const TILE_SIZE: u32 = 48;
 
-pub const MAP_SIZE: isize = 10;
+pub const MAP_SIZE: isize = 20;
 
-pub const N_ANT: usize = 2;
-pub const N_PLANT: usize = 5;
-pub const N_FUNGI: usize = 3;
+pub const N_ANT: usize = 10;
+pub const N_PLANT: usize = 20;
+pub const N_FUNGI: usize = 10;
 
 pub const PHEROMONE_CAPACITY: f32 = 100.0;
