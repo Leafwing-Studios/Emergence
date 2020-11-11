2573720cffcb6202b398042c3b88054f1ae919c7
Alice Cecile <alice.i.cecile@gmail.com>
2020-11-11 16:45:54 -0500

    Moved UI elements into a proper tree structure
    Following: https://github.com/bevyengine/bevy/blob/master/examples/ui/ui.rs

--- asm-lib/src/ui.rs
+++ asm-lib/src/ui.rs
@@ -6,41 +6,70 @@ pub struct UiPlugin;
 impl Plugin for UiPlugin {
 	fn build(&self, app: &mut AppBuilder) {
 		app.add_plugin(FrameTimeDiagnosticsPlugin::default())
-			.add_startup_system(ui_setup.system())
-			.add_startup_system(fps_setup.system())
-			.add_startup_system(pheromones_setup.system())
+			.add_startup_system(build_ui.system())
 			.add_system(update_fps.system())
 			.add_system(update_pheromones.system());
 	}
 }
 
-fn ui_setup(commands: &mut Commands) {
+struct FpsText;
+struct PheromoneText;
+
+fn build_ui(
+	commands: &mut Commands,
+	asset_server: Res<AssetServer>,
+	mut materials: ResMut<Assets<ColorMaterial>>,
+) {
 	commands
 		// 2d camera
-		.spawn(UiCameraComponents::default());
-}
-
-struct FpsText;
-
-fn fps_setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
-	commands
-		// texture
-		.spawn(TextComponents {
+		.spawn(UiCameraComponents::default())
+		// Root node
+		.spawn(NodeComponents {
 			style: Style {
-				align_self: AlignSelf::FlexEnd,
+				size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+				justify_content: JustifyContent::SpaceBetween,
 				..Default::default()
 			},
-			text: Text {
-				value: "FPS:".to_string(),
-				font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-				style: TextStyle {
-					font_size: 60.0,
-					color: Color::WHITE,
-				},
-			},
+			material: materials.add(Color::NONE.into()),
 			..Default::default()
 		})
-		.with(FpsText);
+		.with_children(|parent| {
+			parent
+				// FPS
+				.spawn(TextComponents {
+					style: Style {
+						align_self: AlignSelf::FlexEnd,
+						..Default::default()
+					},
+					text: Text {
+						value: "FPS:".to_string(),
+						font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+						style: TextStyle {
+							font_size: 60.0,
+							color: Color::WHITE,
+						},
+					},
+					..Default::default()
+				})
+				.with(FpsText)
+				// Pheromones
+				.spawn(TextComponents {
+					style: Style {
+						align_self: AlignSelf::FlexEnd,
+						..Default::default()
+					},
+					text: Text {
+						value: "Pheromones:".to_string(),
+						font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+						style: TextStyle {
+							font_size: 60.0,
+							color: Color::WHITE,
+						},
+					},
+					..Default::default()
+				})
+				.with(PheromoneText);
+		});
 }
 
 fn update_fps(diagnostics: Res<Diagnostics>, mut query: Query<(&mut Text, &FpsText)>) {
@@ -51,29 +80,6 @@ fn update_fps(diagnostics: Res<Diagnostics>, mut query: Query<(&mut Text, &FpsTe
 			}
 		}
 	}
-}
-
-struct PheromoneText;
-
-fn pheromones_setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
-	commands
-		// texture
-		.spawn(TextComponents {
-			style: Style {
-				align_self: AlignSelf::Center,
-				..Default::default()
-			},
-			text: Text {
-				value: "Pheromones:".to_string(),
-				font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-				style: TextStyle {
-					font_size: 60.0,
-					color: Color::WHITE,
-				},
-			},
-			..Default::default()
-		})
-		.with(PheromoneText);
 }
 
 fn update_pheromones(pheromones: Res<Pheromones>, mut query: Query<(&mut Text, &PheromoneText)>) {
