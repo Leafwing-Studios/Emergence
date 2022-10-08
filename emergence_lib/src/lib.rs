8188fff8878686fe5a24a1bdb8b66a04614970bc
Alice I. Cecile <alice.i.cecile@gmail.com>
2022-10-08 00:34:29 -0400

    Temporarily disable docs checks

--- emergence_lib/src/lib.rs
+++ emergence_lib/src/lib.rs
@@ -1,5 +1,6 @@
-#![deny(missing_docs)]
-#![deny(clippy::missing_docs_in_private_items)]
+// FIXME: re-enable missing doc checks
+//#![deny(missing_docs)]
+//#![deny(clippy::missing_docs_in_private_items)]
 #![forbid(unsafe_code)]
 #![warn(clippy::doc_markdown)]
 
