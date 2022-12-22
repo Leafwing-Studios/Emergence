08a531dea9b369fa364725ee4e46ddab597a8e36
Lyla <93697517+wallamide@users.noreply.github.com>
2022-12-22 12:07:09 -0600

    Add basic inspector and console functionality (#112)
    * add debugging graphics to a debug_tools crate
    
    * add ui folder to prepare for ui
    
    * Add planning docs as md files
    
    * clean up
    
    * accidental addition of planning docs fix
    
    * updates to debug_ui to attempt toggling ui
    
    * include updated controls
    
    * update to bevy 0.9.1 and fix deps
    
    * fix deps for lib and debug, clean up debug lib
    
    * add basis for console commands
    
    * update console.rs
    
    * fix deps
    
    * finish fixing up debug_tools
    
    * get inspector working again
    
    * rename debug to debug_tools
    
    * fix deps for debug_tools
    
    * add .cargo to gitignore
    
    * updated fps info toggle code and ran clippy
    
    * rename bools to debug_info for clarity
    
    Co-authored-by: TimJentzsch <tim-jentzsch@gmx.de>
    
    * Simplify debug_ui infotext logic
    
    Co-authored-by: TimJentzsch <tim-jentzsch@gmx.de>
    
    * fix tools/debug_tools/src/lib.rs formatting
    
    Co-authored-by: TimJentzsch <tim-jentzsch@gmx.de>
    
    * Update tools/debug_tools/src/debug_ui.rs
    
    Co-authored-by: TimJentzsch <tim-jentzsch@gmx.de>
    
    * update with revisions for pr
    
    * fix naming and logic for toggling fps info
    
    * fix todo in `hive_mind.rs`
    
    * remove cached local toolchain stuff
    
    * fix debug toggle and vsync, add dev controls
    
    * fix lint error
    
    * improve dev mode detection in `hive_mind`
    
    * fix camera.rs to-dos
    
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>
    
    * fix cargo dep rev and clarify `lib` docs.
    
    * Simplify doc string for debug tools plugin
    
    * Simplify doc string for debug tools plugin
    
    * Add doc string for AutoNoVsync
    
    Co-authored-by: Wallamide <wallamide@gamil.com>
    Co-authored-by: TimJentzsch <tim-jentzsch@gmx.de>
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>
    Co-authored-by: Brian Merchant <bhmerchant@gmail.com>

--- emergence_lib/src/hive_mind.rs
+++ emergence_lib/src/hive_mind.rs
@@ -2,9 +2,10 @@
 
 use crate::cursor::CursorTilePos;
 use crate::signals::emitters::Emitter;
-use crate::signals::emitters::StockEmitter::PheromoneAttract;
+use crate::signals::emitters::StockEmitter::{PheromoneAttract, PheromoneRepulse};
 use crate::signals::SignalModificationEvent;
 use bevy::prelude::*;
+use debug_tools::{DebugInfo, DebugToolsPlugin};
 use leafwing_input_manager::prelude::*;
 
 /// Provides the interface between the player and the hive.
@@ -13,8 +14,12 @@ pub struct HiveMindPlugin;
 impl Plugin for HiveMindPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(InputManagerPlugin::<HiveMindAction>::default())
+            .add_plugin(InputManagerPlugin::<DevAction>::default())
+            .init_resource::<DebugInfo>()
+            .add_plugin(DebugToolsPlugin)
             .add_startup_system(initialize_hive_mind)
-            .add_system(place_pheromone);
+            .add_system(place_pheromone)
+            .add_system(show_debug_info);
     }
 }
 
@@ -31,8 +36,56 @@ pub enum HiveMindAction {
     PlaceRepulsivePheromone,
 }
 
+/// Enumerates the actions a developer can take.
+#[derive(Actionlike, Clone, Copy, PartialEq, Eq, Debug)]
+pub enum DevAction {
+    // TODO: make debug labels
+    /// Toggle tilemap labels tools
+    ShowDebugLabels,
+    /// Toggle rendering info
+    ShowInfoText,
+}
+
+// TODO: rework this to use LWIM conventions for mapping controls
+/// Interface for player controls
+pub struct HiveMindControls {
+    /// Place an attractive pheromone
+    pub attractive_pheromone: KeyCode,
+    /// Place a repulsive pheromone
+    pub repulsive_pheromone: UserInput,
+}
+
+/// Add a default control scheme
+impl Default for HiveMindControls {
+    fn default() -> Self {
+        Self {
+            attractive_pheromone: KeyCode::Space,
+            repulsive_pheromone: UserInput::chord([KeyCode::LShift, KeyCode::Space]),
+        }
+    }
+}
+
+// TODO: rework this to use LWIM conventions for mapping controls
+/// Interface for developer controls
+pub struct DevControls {
+    /// Toggle the fps ui
+    pub toggle_fps: KeyCode,
+    // TODO: add more dev controls
+}
+
+/// Add default developer controls
+impl Default for DevControls {
+    fn default() -> Self {
+        Self {
+            toggle_fps: KeyCode::V,
+        }
+    }
+}
+
 /// Startup system initializing the [`HiveMind`].
 fn initialize_hive_mind(mut commands: Commands) {
+    let controls = HiveMindControls::default();
+    let dev_controls = DevControls::default();
     commands
         .spawn_empty()
         .insert(HiveMind)
@@ -40,17 +93,22 @@ fn initialize_hive_mind(mut commands: Commands) {
             action_state: ActionState::default(),
             input_map: InputMap::new([
                 (
-                    KeyCode::Space.into(),
+                    controls.attractive_pheromone.into(),
                     HiveMindAction::PlaceAttractivePheromone,
                 ),
                 (
-                    UserInput::chord([KeyCode::LShift, KeyCode::Space]),
+                    controls.repulsive_pheromone,
                     HiveMindAction::PlaceRepulsivePheromone,
                 ),
             ]),
+        })
+        .insert(InputManagerBundle::<DevAction> {
+            action_state: ActionState::default(),
+            input_map: InputMap::new([(dev_controls.toggle_fps, DevAction::ShowInfoText)]),
         });
 }
 
+// TODO: figure out a different control scheme
 /// Place pheromone, if the mouse is hovered over a hex tile.
 fn place_pheromone(
     mut signal_create_evw: EventWriter<SignalModificationEvent>,
@@ -67,5 +125,33 @@ fn place_pheromone(
             pos: (*cursor_tile_pos).unwrap(),
             increment: 0.1,
         })
+    }
+    // TODO: Fix the failing clamp in curves
+    if hive_mind_state.pressed(HiveMindAction::PlaceRepulsivePheromone)
+        && (*cursor_tile_pos).is_some()
+    {
+        signal_create_evw.send(SignalModificationEvent::SignalIncrement {
+            emitter: Emitter::Stock(PheromoneRepulse),
+            pos: (*cursor_tile_pos).unwrap(),
+            increment: 0.01,
+        });
+    }
+}
+
+/// Toggle showing debug info   
+fn show_debug_info(
+    dev: Query<&ActionState<DevAction>, With<HiveMind>>,
+    mut debug_info: ResMut<DebugInfo>,
+) {
+    let dev = dev.single();
+    let fps_info = dev.pressed(DevAction::ShowInfoText);
+
+    if debug_info.dev_mode {
+        if fps_info && debug_info.show_fps_info {
+            debug_info.show_fps_info = false;
+            info!("fps info toggle")
+        } else if fps_info && !debug_info.show_fps_info {
+            debug_info.show_fps_info = true;
+        }
     }
 }
---
+++ tools/debug_tools/src/lib.rs
@@ -0,0 +1,59 @@
+//! Debugging dev tools for Emergence.
+
+use bevy::{
+    asset::AssetServer,
+    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
+    prelude::{
+        default, Color, Commands, Component, Plugin, Query, Res, Resource, TextBundle, With,
+    },
+    text::{Text, TextSection, TextStyle},
+    time::Time,
+    ui::{PositionType, Style, UiRect, Val},
+};
+
+use bevy_console::*;
+
+use bevy_inspector_egui::WorldInspectorPlugin;
+use console::{print_to_log, PrintToLog};
+
+pub mod console;
+pub mod debug_ui;
+
+/// Creates a global resource that can be used to toggle actively displayed debug tools.
+#[derive(Clone, Resource, Component, Copy, Debug, PartialEq, Eq)]
+pub struct DebugInfo {
+    /// Toggle global access to developer tools
+    pub dev_mode: bool,
+    /// Toggle developer console
+    pub enable_console: bool,
+    /// Toggle the debug tile labels
+    pub show_tile_label: bool,
+    /// Toggle render info
+    pub show_fps_info: bool,
+    /// Toggle displaying the egui inspector
+    pub show_inspector: bool,
+}
+
+impl Default for DebugInfo {
+    fn default() -> Self {
+        Self {
+            dev_mode: true,
+            enable_console: true,
+            show_tile_label: true,
+            show_fps_info: true,
+            show_inspector: true,
+        }
+    }
+}
+/// Adds an instance of `bevy_console`, basic console commands, `bevy-inspector-egui`,
+/// and basic performance information like fps and frame counting.
+pub struct DebugToolsPlugin;
+
+impl Plugin for DebugToolsPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_plugin(WorldInspectorPlugin::new())
+            .add_plugin(FrameTimeDiagnosticsPlugin)
+            .add_plugin(ConsolePlugin)
+            .add_console_command::<PrintToLog, _>(print_to_log);
+    }
+}
