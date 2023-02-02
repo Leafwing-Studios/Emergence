a4325e35236bc4137735f7bcb56204090416bba4
Alice Cecile <alice.i.cecile@gmail.com>
2023-02-02 14:50:42 -0500

    Improve camera controls (#261)
    * Remove example from hexx
    
    * Increase map size
    
    * Simplify starting camera position (for now)
    
    * Add basic rotation controls
    
    * Cleanup
    
    * Use camera-relative motion
    
    * Swap to an angled view
    
    * Add hexx::Direction clockwise and counterclockwise
    
    * Add simple Facing component for discretized rotation
    
    * Simple snap rotation for facing
    
    * Split camera and non-camera facing systems
    
    * Better paramaterization for starting camera config
    
    * Rotate around the camera's focus
    
    * I guess we're y-up lol
    
    * Always pan on the XZ plane
    
    * Set min and max zoom
    
    * Interpolate camera  movement for smooth transitions
    
    * Pan rate should be proportional to zoom

--- emergence_lib/src/player_interaction/camera.rs
+++ emergence_lib/src/player_interaction/camera.rs
@@ -1,8 +1,8 @@
 //! Camera controls and movement.
 //!
-//! This RTS-style camera can zoom and pan.
+//! This RTS-style camera can zoom, pan and rotate.
 
-// use std::f32::consts::FRAC_PI_3;
+use std::f32::consts::PI;
 
 use bevy::prelude::*;
 use leafwing_input_manager::axislike::SingleAxis;
@@ -13,6 +13,11 @@ use leafwing_input_manager::prelude::VirtualDPad;
 use leafwing_input_manager::Actionlike;
 use leafwing_input_manager::InputManagerBundle;
 
+use crate::simulation::geometry::angle;
+use crate::simulation::geometry::clockwise;
+use crate::simulation::geometry::counterclockwise;
+use crate::simulation::geometry::Facing;
+
 use super::InteractionSystem;
 
 /// Camera logic
@@ -22,32 +27,43 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(InputManagerPlugin::<CameraAction>::default())
             .add_startup_system_to_stage(StartupStage::Startup, setup)
-            .add_system(camera_movement.label(InteractionSystem::MoveCamera));
+            .add_system(
+                rotate_camera
+                    .label(InteractionSystem::MoveCamera)
+                    // We rely on the updated focus information from this system
+                    .after(translate_camera),
+            )
+            .add_system(translate_camera.label(InteractionSystem::MoveCamera));
     }
 }
 
-/// Sets the angle the camera is oriented around the z-axis.
-const CAM_ANG: f32 = 0.733;
+/// The distance from the origin that the camera begins at.
+///
+/// Should be between the default values of [`CameraSettings`] `min_zoom` and `max_zoom`.
+const STARTING_DISTANCE_FROM_ORIGIN: f32 = 30.;
+
+/// The angle in radians that the camera forms with the ground.
+///
+/// This value should be between 0 (horizontal) and PI / 2 (vertical).
+const CAMERA_ANGLE: f32 = PI / 4.;
 
 /// Spawns a [`Camera3dBundle`] and sets up the [`InputManagerBundle`]s that handle camera motion
 fn setup(mut commands: Commands) {
-    // FIXME: swap to z-up coordinates. Blocked on https://github.com/ManevilleF/hexx/issues/10
     commands
-        .spawn(Camera3dBundle {
-            transform: Transform::from_xyz(20.0, 6.0, 30.0)
-                .looking_at(Vec3::ZERO, -Vec3::Y)
-                .with_rotation(Quat::from_euler(EulerRot::XZY, 0., 0., CAM_ANG)),
-            ..Default::default()
-        })
+        .spawn(Camera3dBundle::default())
         .insert(InputManagerBundle::<CameraAction> {
             input_map: InputMap::default()
                 .insert(VirtualDPad::wasd(), CameraAction::Pan)
                 .insert(VirtualDPad::arrow_keys(), CameraAction::Pan)
                 .insert(SingleAxis::mouse_wheel_y(), CameraAction::Zoom)
+                .insert(KeyCode::Q, CameraAction::RotateLeft)
+                .insert(KeyCode::E, CameraAction::RotateRight)
                 .build(),
             ..default()
         })
-        .insert(CameraSettings::default());
+        .insert(CameraSettings::default())
+        .insert(CameraFocus::default())
+        .insert(Facing::default());
 }
 
 /// Actions that manipulate the camera
@@ -57,6 +73,32 @@ enum CameraAction {
     Pan,
     /// Reveal more or less of the map by pulling the camera away or moving it closer
     Zoom,
+    /// Rotates the camera counterclockwise
+    RotateLeft,
+    /// Rotates the camera clockwise
+    RotateRight,
+}
+
+/// The position that the camera is looking at.
+///
+/// When panning and zooming, this struct is updated, rather than modifying the camera's [`Transform`] directly.
+#[derive(Component, Debug)]
+struct CameraFocus {
+    /// The coordinate that the camera is looking at.
+    ///
+    /// This should be the top of the column at the center of the screen.
+    translation: Vec3,
+    /// The distance from the focus to the camera.
+    zoom: f32,
+}
+
+impl Default for CameraFocus {
+    fn default() -> Self {
+        CameraFocus {
+            translation: Vec3::ZERO,
+            zoom: STARTING_DISTANCE_FROM_ORIGIN,
+        }
+    }
 }
 
 /// Configure how the camera moves and feels.
@@ -70,51 +112,130 @@ struct CameraSettings {
     ///
     /// Should always be positive.
     pan_speed: f32,
+    /// The minimum distance that the camera can be from its focus.
+    ///
+    /// Should always be positive, and less than `max_zoom`.
+    min_zoom: f32,
+    /// The maximum distance that the camera can be from its focus.
+    ///
+    /// Should always be positive, and less than `max_zoom`.
+    max_zoom: f32,
+    /// The linear interpolation coefficient for camera movement.
+    ///
+    /// Should always be between 0 (unmoving) and 1 (instant).
+    linear_interpolation: f32,
+    /// The spherical linear interpolation coefficient for camera rotation.
+    ///
+    /// Should always be between 0 (unmoving) and 1 (instant).
+    rotational_interpolation: f32,
 }
 
 impl Default for CameraSettings {
     fn default() -> Self {
         CameraSettings {
             zoom_speed: 500.,
-            pan_speed: 50.,
+            pan_speed: 1.,
+            min_zoom: 2.,
+            max_zoom: 100.,
+            linear_interpolation: 0.2,
+            rotational_interpolation: 0.1,
         }
     }
 }
 
-/// The scale that controls the amount the camera will move in the x direction
-const ZOOM_PAN_SCALE: f32 = 0.5;
-
-/// Handles camera motion
-fn camera_movement(
+/// Pan and zoom the camera
+fn translate_camera(
     mut camera_query: Query<
-        (&mut Transform, &ActionState<CameraAction>, &CameraSettings),
+        (
+            &mut CameraFocus,
+            &Facing,
+            &ActionState<CameraAction>,
+            &CameraSettings,
+        ),
         With<Camera3d>,
     >,
     time: Res<Time>,
 ) {
-    let (mut camera_transform, camera_actions, settings) = camera_query.single_mut();
+    let (mut focus, facing, camera_actions, settings) = camera_query.single_mut();
 
     // Zoom
     if camera_actions.pressed(CameraAction::Zoom) {
-        // FIXME: swap to z-up
-        let camera_actions = camera_actions.value(CameraAction::Zoom);
-        let delta_x = camera_actions * time.delta_seconds() * settings.zoom_speed * ZOOM_PAN_SCALE;
-        let delta_y = camera_actions * time.delta_seconds() * settings.pan_speed;
-        // oriented from the POV that you're the player trying to zoom in to the game map
-        camera_transform.translation.y -= delta_y;
+        let delta_zoom =
+            -camera_actions.value(CameraAction::Zoom) * time.delta_seconds() * settings.zoom_speed;
 
-        camera_transform.translation.x -= delta_x;
-        camera_transform.translation.z -= delta_x;
+        // Zoom in / out on whatever we're looking at
+        focus.zoom = (focus.zoom + delta_zoom).clamp(settings.min_zoom, settings.max_zoom);
     }
 
     // Pan
     if camera_actions.pressed(CameraAction::Pan) {
         let dual_axis_data = camera_actions.axis_pair(CameraAction::Pan).unwrap();
-        let delta_x = dual_axis_data.x() * time.delta_seconds() * settings.pan_speed;
-        let delta_y = dual_axis_data.y() * time.delta_seconds() * settings.pan_speed;
+        let base_xy = dual_axis_data.xy();
+        let scaled_xy = base_xy * time.delta_seconds() * settings.pan_speed * focus.zoom;
+        // Plane is XZ, but gamepads are XY
+        let unoriented_translation = Vec3 {
+            x: scaled_xy.y,
+            y: 0.,
+            z: scaled_xy.x,
+        };
 
-        camera_transform.translation.x += delta_x;
-        // FIXME: swap to z-up
-        camera_transform.translation.z -= delta_y;
+        let facing_angle = angle(facing.direction);
+        let rotation = Quat::from_rotation_y(facing_angle);
+        let oriented_translation = rotation.mul_vec3(unoriented_translation);
+
+        focus.translation += oriented_translation;
     }
+}
+
+/// Rotates the camera around the [`CameraFocus`].
+fn rotate_camera(
+    mut query: Query<
+        (
+            &mut Transform,
+            &mut Facing,
+            &CameraFocus,
+            &CameraSettings,
+            &ActionState<CameraAction>,
+        ),
+        With<Camera3d>,
+    >,
+) {
+    let (mut transform, mut facing, focus, settings, camera_actions) = query.single_mut();
+
+    // Set facing
+    if camera_actions.just_pressed(CameraAction::RotateLeft) {
+        facing.direction = counterclockwise(facing.direction);
+    }
+
+    if camera_actions.just_pressed(CameraAction::RotateRight) {
+        facing.direction = clockwise(facing.direction);
+    }
+
+    // Goal: move the camera around a central point
+
+    // Always begin due "south" of the focus.
+    let mut new_transform =
+        Transform::from_translation(focus.translation + Vec3::NEG_X * focus.zoom);
+
+    // Tilt up
+    new_transform.translate_around(
+        focus.translation,
+        Quat::from_axis_angle(Vec3::NEG_Z, CAMERA_ANGLE),
+    );
+
+    // Rotate around on the xz plane
+    let planar_angle = angle(facing.direction);
+    new_transform.translate_around(focus.translation, Quat::from_rotation_y(planar_angle));
+
+    // Look at that central point
+    new_transform.look_at(focus.translation, Vec3::Y);
+
+    // Replace the previous transform
+    // Use lerping to smooth the transition
+    transform.translation = transform
+        .translation
+        .lerp(new_transform.translation, settings.linear_interpolation);
+    transform.rotation = transform
+        .rotation
+        .slerp(new_transform.rotation, settings.rotational_interpolation)
 }
--- emergence_lib/src/simulation/geometry.rs
+++ emergence_lib/src/simulation/geometry.rs
@@ -1,10 +1,9 @@
 //! Manages the game world's grid and data tied to that grid
 
-use bevy::{
-    prelude::{Component, Deref, DerefMut, Entity, Resource},
-    utils::HashMap,
-};
-use hexx::{Hex, HexLayout};
+use std::f32::consts::PI;
+
+use bevy::{prelude::*, utils::HashMap};
+use hexx::{Direction, Hex, HexLayout};
 
 /// A hex-based coordinate, that represents exactly one tile.
 #[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Hash, Deref, DerefMut)]
@@ -32,9 +31,114 @@ impl Default for MapGeometry {
     fn default() -> Self {
         MapGeometry {
             layout: HexLayout::default(),
-            radius: 10,
+            radius: 50,
             tiles_index: HashMap::default(),
             structure_index: HashMap::default(),
+        }
+    }
+}
+
+/// The hex direction that this entity is facing.
+///
+/// Stored as a component on each entity with a grid-aligned rotation.
+#[derive(Component, Clone, Copy, Debug, PartialEq, Eq, Deref, DerefMut)]
+pub struct Facing {
+    /// The desired direction.
+    ///
+    /// Defaults to [`Direction::Top`].
+    pub direction: Direction,
+}
+
+impl Default for Facing {
+    fn default() -> Self {
+        Facing {
+            direction: Direction::Top,
+        }
+    }
+}
+
+/// Rotates objects so they are facing the correct direction.
+pub(super) fn sync_rotation_to_facing(
+    // Camera requires different logic, it rotates "around" a central point
+    mut query: Query<(&mut Transform, &Facing), (Changed<Facing>, Without<Camera3d>)>,
+) {
+    for (mut transform, &facing) in query.iter_mut() {
+        // Rotate the object in the correct direction
+        let target = Quat::from_axis_angle(Vec3::Y, angle(facing.direction));
+        transform.rotation = target;
+    }
+}
+
+/// Rotates a hex [`Direction`] one step clockwise.
+#[must_use]
+pub fn clockwise(direction: Direction) -> Direction {
+    use Direction::*;
+    match direction {
+        BottomRight => Bottom,
+        TopRight => BottomRight,
+        Top => TopRight,
+        TopLeft => Top,
+        BottomLeft => TopLeft,
+        Bottom => BottomLeft,
+    }
+}
+
+/// Rotates a hex [`Direction`] one step counterclockwise.
+#[must_use]
+pub fn counterclockwise(direction: Direction) -> Direction {
+    use Direction::*;
+    match direction {
+        BottomRight => TopRight,
+        TopRight => Top,
+        Top => TopLeft,
+        TopLeft => BottomLeft,
+        BottomLeft => Bottom,
+        Bottom => BottomRight,
+    }
+}
+
+/// Returns the angle associated with the provided hex [`Direction`].
+///
+/// Measured in radians counterclockwise from the +x axis.
+pub fn angle(direction: Direction) -> f32 {
+    use Direction::*;
+    // See https://dr282zn36sxxg.cloudfront.net/datastreams/f-d%3Adff233ddd1e7e4c34a6545a8dfc1d63bbaf7eefbb40215febc633a30%2BIMAGE_TINY%2BIMAGE_TINY.1
+    PI / 6.
+        * match direction {
+            BottomRight => 11.,
+            TopRight => 1.,
+            Top => 3.,
+            TopLeft => 5.,
+            BottomLeft => 7.,
+            Bottom => 9.,
+        }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rotations_reverse_each_other() {
+        for direction in Direction::ALL_DIRECTIONS {
+            assert_eq!(direction, counterclockwise(clockwise(direction)));
+            assert_eq!(direction, clockwise(counterclockwise(direction)));
+        }
+    }
+
+    #[test]
+    fn six_rotations_comes_home() {
+        for direction in Direction::ALL_DIRECTIONS {
+            let mut cw_direction = direction;
+            let mut ccw_direction = direction;
+
+            for _ in 0..6 {
+                cw_direction = clockwise(cw_direction);
+                ccw_direction = counterclockwise(ccw_direction);
+            }
+
+            assert_eq!(direction, cw_direction);
+            assert_eq!(direction, ccw_direction);
         }
     }
 }
