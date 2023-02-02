060d83fde8e38686408754adad3d174087aec7b6
Alice Cecile <alice.i.cecile@gmail.com>
2023-02-02 11:06:11 -0500

    Add hexx::Direction clockwise and counterclockwise

--- emergence_lib/src/simulation/geometry.rs
+++ emergence_lib/src/simulation/geometry.rs
@@ -4,7 +4,7 @@ use bevy::{
     prelude::{Component, Deref, DerefMut, Entity, Resource},
     utils::HashMap,
 };
-use hexx::{Hex, HexLayout};
+use hexx::{Direction, Hex, HexLayout};
 
 /// A hex-based coordinate, that represents exactly one tile.
 #[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Hash, Deref, DerefMut)]
@@ -35,6 +35,63 @@ impl Default for MapGeometry {
             radius: 50,
             tiles_index: HashMap::default(),
             structure_index: HashMap::default(),
+        }
+    }
+}
+
+/// Rotates a hex [`Direction`] one step clockwise.
+#[must_use]
+pub fn clockwise(direction: Direction) -> Direction {
+    use Direction::*;
+    match direction {
+        BottomRight => Bottom,
+        TopRight => BottomRight,
+        Top => TopRight,
+        TopLeft => Top,
+        BottomLeft => TopLeft,
+        Bottom => BottomLeft,
+    }
+}
+
+/// Rotates a hex [`Direction`] one step counterclockwise.
+#[must_use]
+pub fn counterclockwise(direction: Direction) -> Direction {
+    use Direction::*;
+    match direction {
+        BottomRight => TopRight,
+        TopRight => Top,
+        Top => TopLeft,
+        TopLeft => BottomLeft,
+        BottomLeft => Bottom,
+        Bottom => BottomRight,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rotations_reverse_each_other() {
+        for direction in Direction::ALL_DIRECTIONS {
+            assert_eq!(direction, counterclockwise(clockwise(direction)));
+            assert_eq!(direction, clockwise(counterclockwise(direction)));
+        }
+    }
+
+    #[test]
+    fn six_rotations_comes_home() {
+        for direction in Direction::ALL_DIRECTIONS {
+            let mut cw_direction = direction;
+            let mut ccw_direction = direction;
+
+            for _ in 0..6 {
+                cw_direction = clockwise(cw_direction);
+                ccw_direction = counterclockwise(ccw_direction);
+            }
+
+            assert_eq!(direction, cw_direction);
+            assert_eq!(direction, ccw_direction);
         }
     }
 }
