4352ec422c753f37ca53f3a454c21a3679da170a
Alice Cecile <alice.i.cecile@gmail.com>
2023-02-10 23:24:48 -0500

    Attempt to set materials of ghost children

--- emergence_lib/src/graphics/mod.rs
+++ emergence_lib/src/graphics/mod.rs
@@ -22,14 +22,16 @@ pub struct GraphicsPlugin;
 
 impl Plugin for GraphicsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(LightingPlugin).add_system_set(
-            SystemSet::on_update(AssetState::Ready)
-                .with_system(populate_terrain)
-                .with_system(populate_units)
-                .with_system(populate_structures)
-                // We need to avoid attempting to insert bundles into entities that no longer exist
-                .with_system(mesh_ghosts.before(InteractionSystem::ManageGhosts)),
-        );
+        app.add_plugin(LightingPlugin)
+            .add_system_set(
+                SystemSet::on_update(AssetState::Ready)
+                    .with_system(populate_terrain)
+                    .with_system(populate_units)
+                    .with_system(populate_structures)
+                    // We need to avoid attempting to insert bundles into entities that no longer exist
+                    .with_system(mesh_ghosts.before(InteractionSystem::ManageGhosts)),
+            )
+            .add_system_to_stage(CoreStage::PostUpdate, change_ghost_material);
     }
 }
 
@@ -92,11 +94,32 @@ fn mesh_ghosts(
 
         let scene_handle = structure_handles.scenes.get(structure_id).unwrap();
 
-        commands.entity(entity).insert(SceneBundle {
-            scene: scene_handle.clone_weak(),
-            transform: Transform::from_xyz(pos.x, terrain_height + StructureId::OFFSET, pos.y),
-            ..default()
-        });
+        // Spawns as a child of the root ghost
+        commands.entity(entity).insert((
+            // Every entity in this hierarchy will have this marker component
+            GhostSceneMarker,
+            SceneBundle {
+                scene: scene_handle.clone_weak(),
+                transform: Transform::from_xyz(pos.x, terrain_height + StructureId::OFFSET, pos.y),
+                ..default()
+            },
+        ));
+    }
+}
+
+/// Marker for entities spawned as part of a scene.
+///
+/// Child of a [`Ghost`]-holding entity.
+#[derive(Component)]
+struct GhostSceneMarker;
+
+/// Modifies the material of any entities spawned due to a ghost structure.
+fn change_ghost_material(
+    mut query: Query<&mut Handle<StandardMaterial>, Added<GhostSceneMarker>>,
+    structure_handles: Res<StructureHandles>,
+) {
+    for mut material in query.iter_mut() {
+        *material = structure_handles.ghost_material.clone_weak();
     }
 }
 
