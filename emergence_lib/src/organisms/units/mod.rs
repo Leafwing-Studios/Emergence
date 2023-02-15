30fcaa374f66ae9201edcae98d49ce7817508c14
Alice Cecile <alice.i.cecile@gmail.com>
2023-02-15 14:27:34 -0500

    Remove dead code

--- emergence_lib/src/organisms/units/mod.rs
+++ emergence_lib/src/organisms/units/mod.rs
@@ -63,9 +63,7 @@ pub enum UnitSystem {
 pub struct UnitsPlugin;
 impl Plugin for UnitsPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(UnitTimer(Timer::from_seconds(0.5, TimerMode::Repeating)))
-            .insert_resource(SignalTransducer::<BottomClampedLine>::default())
-            .add_system(behavior::advance_action_timer.label(UnitSystem::AdvanceTimers))
+        app.add_system(behavior::advance_action_timer.label(UnitSystem::AdvanceTimers))
             .add_system(
                 movement::move_units
                     .label(UnitSystem::Act)
@@ -78,67 +76,5 @@ impl Plugin for UnitsPlugin {
                     .after(UnitSystem::Act)
                     .after(UnitSystem::ChooseGoal),
             );
-    }
-}
-
-/// Global timer that controls when units should act
-#[derive(Resource, Debug)]
-struct UnitTimer(Timer);
-
-/// Transforms a signal into a weight used to make decisions.
-///
-/// The transduction is modelled by mapping the signal to a weight using a curve.
-#[derive(Resource)]
-pub struct SignalTransducer<C: Mapping> {
-    /// Curve used to model transduction.
-    curve: C,
-}
-
-impl SignalTransducer<Sigmoid> {
-    /// Creates a [`Sigmoid`]-based transducer.
-    pub fn new(
-        min: f32,
-        max: f32,
-        first_percentile: f32,
-        last_percentile: f32,
-    ) -> SignalTransducer<Sigmoid> {
-        SignalTransducer {
-            curve: Sigmoid::new(min, max, first_percentile, last_percentile),
-        }
-    }
-
-    /// Transduce a signal into a weight used for decision making.
-    pub fn signal_to_weight(&self, attraction: f32, repulsion: f32) -> f32 {
-        1.0 + self.curve.map(attraction) - self.curve.map(repulsion)
-    }
-}
-
-impl Default for SignalTransducer<Sigmoid> {
-    fn default() -> Self {
-        SignalTransducer {
-            curve: Sigmoid::new(0.0, 0.1, 0.01, 0.09),
-        }
-    }
-}
-
-impl SignalTransducer<BottomClampedLine> {
-    /// Creates a [`BottomClampedLine`]-based transducer.
-    pub fn new(p0: Vec2, p1: Vec2) -> SignalTransducer<BottomClampedLine> {
-        SignalTransducer {
-            curve: BottomClampedLine::new_from_points(p0, p1),
-        }
-    }
-
-    /// Transduce a signal into a weight used for decision making.
-    pub fn signal_to_weight(&self, attraction: f32, repulsion: f32) -> f32 {
-        1.0 + self.curve.map(attraction) - self.curve.map(repulsion)
-    }
-}
-
-impl Default for SignalTransducer<BottomClampedLine> {
-    fn default() -> Self {
-        SignalTransducer {
-            curve: BottomClampedLine::new_from_points(Vec2::new(0.0, 0.0), Vec2::new(0.01, 1.0)),
-        }
     }
 }
