998ff7989ea81dbb18944e3dde73e1803f266a47
Alice Cecile <alice.i.cecile@gmail.com>
2023-02-21 09:28:29 -0500

    Remove dead code for using materials to show selections

--- emergence_lib/src/graphics/structures.rs
+++ emergence_lib/src/graphics/structures.rs
@@ -4,9 +4,8 @@ use bevy::prelude::*;
 
 use crate::{
     asset_management::structures::StructureHandles,
-    player_interaction::selection::ObjectInteraction,
     simulation::geometry::{MapGeometry, TilePos},
-    structures::{ghost::Ghostly, StructureId},
+    structures::StructureId,
 };
 
 /// Adds rendering components to every spawned structure, real or otherwise
@@ -30,31 +29,5 @@ pub(super) fn populate_structures(
                 ..default()
             })
             .insert(structure_handles.picking_mesh.clone_weak());
-    }
-}
-
-/// Modifies the material of any structures based on their interaction state.
-pub(super) fn change_structure_material(
-    root_structure_query: Query<(Entity, &ObjectInteraction, Option<&Ghostly>), With<StructureId>>,
-    children: Query<&Children>,
-    mut material_query: Query<&mut Handle<StandardMaterial>>,
-    structure_handles: Res<StructureHandles>,
-) {
-    for (root_entity, object_interaction, maybe_ghostly) in root_structure_query.iter() {
-        for child in children.iter_descendants(root_entity) {
-            if let Ok(mut material) = material_query.get_mut(child) {
-                let maybe_material_handle = match maybe_ghostly {
-                    Some(..) => structure_handles.ghost_materials.get(object_interaction),
-                    None => structure_handles
-                        .interaction_materials
-                        .get(object_interaction),
-                };
-
-                // FIXME: how do we restore the materials back to their original form??
-                if let Some(new_handle) = maybe_material_handle {
-                    *material = new_handle.clone_weak();
-                }
-            }
-        }
     }
 }
