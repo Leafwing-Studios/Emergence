620fa2b2ea8fc7f81f64f7c6d36cdb51aef62a15
Alice Cecile <alice.i.cecile@gmail.com>
2023-02-23 16:08:57 -0500

    Handle UnitAction::Eat

--- emergence_lib/src/units/hunger.rs
+++ emergence_lib/src/units/hunger.rs
@@ -2,13 +2,17 @@
 
 use bevy::prelude::*;
 use core::fmt::Display;
+use leafwing_abilities::prelude::Pool;
 
 use crate::{
-    items::ItemId,
+    items::{ItemCount, ItemId},
     organisms::energy::{Energy, EnergyPool},
 };
 
-use super::behavior::Goal;
+use super::{
+    behavior::{CurrentAction, Goal, UnitAction},
+    item_interaction::HeldItem,
+};
 
 /// The item(s) that a unit must consume to gain [`Energy`].
 #[derive(Component, Clone, Debug)]
@@ -37,6 +41,28 @@ pub(super) fn check_for_hunger(mut unit_query: Query<(&mut Goal, &EnergyPool, &D
     for (mut goal, energy_pool, diet) in unit_query.iter_mut() {
         if energy_pool.should_warn() {
             *goal = Goal::Eat(diet.item);
+        }
+    }
+}
+
+/// Swaps the goal to [`Goal::Eat`] when energy is low
+pub(super) fn eat_held_items(
+    mut unit_query: Query<(&CurrentAction, &mut HeldItem, &Diet, &mut EnergyPool)>,
+) {
+    for (current_action, mut held_item, diet, mut energy_pool) in unit_query.iter_mut() {
+        if let UnitAction::Eat = current_action.action() {
+            let item_count = ItemCount::new(diet.item, 1);
+            let consumption_result = held_item.remove_item_all_or_nothing(&item_count);
+
+            match consumption_result {
+                Ok(_) => {
+                    let proposed = energy_pool.current() + diet.energy;
+                    energy_pool.set_current(proposed);
+                }
+                Err(error) => {
+                    error!("{error:?}: unit tried to eat the wrong thing!")
+                }
+            }
         }
     }
 }
