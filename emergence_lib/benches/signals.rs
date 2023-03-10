0b9c881985e4bf82a8754e60873f87b31f20be22
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-10 13:42:54 -0500

    Add more benchmarks at different sample sizes

--- emergence_lib/benches/signals.rs
+++ emergence_lib/benches/signals.rs
@@ -5,15 +5,15 @@ use emergence_lib::simulation::geometry::{MapGeometry, TilePos};
 use rand::thread_rng;
 
 /// Setup function
-fn add_signals(map_radius: u32, n_signals: u64, n_sources: u32) -> (Signals, MapGeometry) {
+fn add_signals(settings: Settings) -> (Signals, MapGeometry) {
     let mut signals = Signals::default();
-    let map_geometry = MapGeometry::new(map_radius);
+    let map_geometry = MapGeometry::new(settings.map_radius);
     let mut rng = thread_rng();
 
-    for i in 0..n_signals {
+    for i in 0..settings.n_signals {
         let signal_type = SignalType::Pull(Id::new(i));
 
-        for _ in 0..n_sources {
+        for _ in 0..settings.n_sources {
             let tile_pos = TilePos::random(&map_geometry, &mut rng);
 
             signals.add_signal(signal_type, tile_pos, SignalStrength::new(1.));
@@ -24,15 +24,76 @@ fn add_signals(map_radius: u32, n_signals: u64, n_sources: u32) -> (Signals, Map
 }
 
 /// Benchmarks the signal diffusion process
-fn signal_diffusion(map_radius: u32, n_signals: u64, n_sources: u32) {
-    let (mut signals, map_geometry) = add_signals(map_radius, n_signals, n_sources);
+fn signal_diffusion(settings: Settings) {
+    let (mut signals, map_geometry) = add_signals(settings);
     signals.diffuse(&map_geometry, DIFFUSION_FRACTION);
 }
 
+/// Benchmark settings, in a reusable form
+struct Settings {
+    map_radius: u32,
+    n_signals: u64,
+    n_sources: u32,
+}
+
+impl Settings {
+    /// Controls how closely packed emitting structures are.
+    ///
+    /// Realistic values are between 2 and 20 or so.
+    const SPARSITY: u32 = 5;
+
+    const MINIMAL: Settings = Settings {
+        map_radius: 1,
+        n_signals: 1,
+        n_sources: 1,
+    };
+
+    const TINY: Settings = Settings {
+        map_radius: 10,
+        n_signals: 10,
+        n_sources: 10 * 10 / Self::SPARSITY,
+    };
+
+    const MODEST: Settings = Settings {
+        map_radius: 100,
+        n_signals: 100,
+        n_sources: 100 * 100 / Self::SPARSITY,
+    };
+
+    const HUGE: Settings = Settings {
+        map_radius: 1000,
+        n_signals: 1000,
+        n_sources: 1000 * 1000 / Self::SPARSITY,
+    };
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("add_signal_minimal", |b| b.iter(|| add_signals(1, 1, 1)));
+    c.bench_function("add_signal_minimal", |b| {
+        b.iter(|| add_signals(Settings::MINIMAL))
+    });
     c.bench_function("signal_diffusion_minimal", |b| {
-        b.iter(|| signal_diffusion(1, 1, 1))
+        b.iter(|| signal_diffusion(Settings::MINIMAL))
+    });
+
+    c.bench_function("add_signal_tiny", |b| {
+        b.iter(|| add_signals(Settings::TINY))
+    });
+    c.bench_function("signal_diffusion_tiny", |b| {
+        b.iter(|| signal_diffusion(Settings::TINY))
+    });
+
+    c.bench_function("add_signal_modest", |b| {
+        b.iter(|| add_signals(Settings::MODEST))
+    });
+    c.bench_function("signal_diffusion_modest", |b| {
+        b.iter(|| signal_diffusion(Settings::MODEST))
+    });
+
+    c.bench_function("add_signal_huge", |b| {
+        b.iter(|| add_signals(Settings::HUGE))
+    });
+    c.bench_function("signal_diffusion_huge", |b| {
+        b.iter(|| signal_diffusion(Settings::HUGE))
     });
 }
 
