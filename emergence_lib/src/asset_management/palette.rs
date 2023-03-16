38cc099493d09de69252af8ede78768013204f54
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-16 19:18:49 -0400

    Make tile selection translucent

--- emergence_lib/src/asset_management/palette.rs
+++ emergence_lib/src/asset_management/palette.rs
@@ -2,6 +2,9 @@
 
 use bevy::prelude::Color;
 
+/// The alpha value used for selection/hovering/other UI overlay
+const OVERLAY_ALPHA: f32 = 0.5;
+
 /// The hue of selected objects
 pub(crate) const SELECTION_HUE: f32 = 100.;
 /// The saturation of selected objects
@@ -9,8 +12,12 @@ pub(crate) const SELECTION_SATURATION: f32 = 0.5;
 /// The lightness of selected objects
 pub(crate) const SELECTION_LIGHTNESS: f32 = 0.6;
 /// The color used to tint selected objects.
-pub(crate) const SELECTION_COLOR: Color =
-    Color::hsl(SELECTION_HUE, SELECTION_SATURATION, SELECTION_LIGHTNESS);
+pub(crate) const SELECTION_COLOR: Color = Color::hsla(
+    SELECTION_HUE,
+    SELECTION_SATURATION,
+    SELECTION_LIGHTNESS,
+    OVERLAY_ALPHA,
+);
 
 /// The hue used to indicate that an action is forbidden.
 pub(crate) const FORBIDDEN_HUE: f32 = 0.;
@@ -21,8 +28,10 @@ pub(crate) const HOVER_HUE: f32 = 55.;
 pub(crate) const HOVER_SATURATION: f32 = 0.5;
 /// The lightness of selected objects
 pub(crate) const HOVER_LIGHTNESS: f32 = 0.6;
+
 /// The color used to tint hovered objects.
-pub(crate) const HOVER_COLOR: Color = Color::hsl(HOVER_HUE, HOVER_SATURATION, HOVER_LIGHTNESS);
+pub(crate) const HOVER_COLOR: Color =
+    Color::hsla(HOVER_HUE, HOVER_SATURATION, HOVER_LIGHTNESS, OVERLAY_ALPHA);
 
 /// The hue value of ghost-like materials.
 pub(crate) const GHOST_HUE: f32 = 0.0;
@@ -55,10 +64,11 @@ pub(crate) const FORBIDDEN_PREVIEW_COLOR: Color = Color::hsla(
 );
 
 /// The color used to tint objects that are both selected and hovered.
-pub(crate) const SELECTION_AND_HOVER_COLOR: Color = Color::hsl(
+pub(crate) const SELECTION_AND_HOVER_COLOR: Color = Color::hsla(
     (SELECTION_HUE + HOVER_HUE) / 2.,
     (SELECTION_SATURATION + HOVER_SATURATION) / 2.,
     (SELECTION_LIGHTNESS + HOVER_LIGHTNESS) / 2.,
+    OVERLAY_ALPHA,
 );
 
 /// The color used for columns of dirt underneath tiles
