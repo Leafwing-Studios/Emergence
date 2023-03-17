6b7633d738a200a07d230c1de1e157ba7c6aa8bc
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-17 15:42:00 -0400

    Position sun using the power of TRIGONOMETRY

--- emergence_lib/src/graphics/lighting.rs
+++ emergence_lib/src/graphics/lighting.rs
@@ -1,8 +1,13 @@
 //! Lights and lighting.
 
-use bevy::{pbr::CascadeShadowConfigBuilder, prelude::*};
+use std::f32::consts::PI;
 
-use crate::asset_management::palette::LIGHT_SUN;
+use bevy::{
+    pbr::{CascadeShadowConfigBuilder, DirectionalLightShadowMap},
+    prelude::*,
+};
+
+use crate::{asset_management::palette::LIGHT_SUN, simulation::geometry::Height};
 
 /// Handles all lighting logic
 pub(super) struct LightingPlugin;
@@ -13,12 +18,38 @@ impl Plugin for LightingPlugin {
             brightness: 1.0,
             color: LIGHT_SUN,
         })
+        .insert_resource(DirectionalLightShadowMap { size: 4096 })
         .add_startup_system(spawn_sun);
     }
 }
 
 /// Spawns a directional light source to illuminate the scene
 fn spawn_sun(mut commands: Commands) {
+    // The distance from the sun to the origin
+    let sun_height = 2. * Height::MAX.into_world_pos();
+    // The angle of the sun, relative to its position at noon
+    // The sun is aligned such that -x is west, +x is east
+    // This will change throughout the day, from - PI/2 to PI/2
+    let sun_angle = PI / 4.;
+
+    /// The angle that the sun is offset from vertical
+    ///
+    /// This technically changes with latitude.
+    /// Should be 0 at the equator and
+    const ELEVATION_ANGLE: f32 = 23.5 / 360. * PI / 2.;
+
+    // Set the height
+    let mut transform = Transform::from_xyz(0., sun_height, 0.);
+
+    // Offset by the sun angle, then the elevation angle,
+    transform.rotate_around(
+        Vec3::ZERO,
+        Quat::from_euler(EulerRot::XZY, sun_angle, ELEVATION_ANGLE, 0.),
+    );
+
+    // Look at the origin to point in the right direction
+    transform.look_at(Vec3::ZERO, Vec3::Y);
+
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             color: LIGHT_SUN,
@@ -34,7 +65,7 @@ fn spawn_sun(mut commands: Commands) {
             overlap_proportion: 0.5,
         }
         .build(),
-        transform: Transform::from_xyz(30., 100., 30.).looking_at(Vec3::ZERO, Vec3::Y),
+        transform,
         ..default()
     });
 }
