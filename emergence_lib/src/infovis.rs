fccdd2cf802381e6d92994b58d3c2d8ff27900f9
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-18 19:54:34 -0400

    Add a simple population display (#540)
    * Add simple census system
    
    * Skeleton for population display
    
    * Make text more visible
    
    * Docs

---
+++ emergence_lib/src/infovis.rs
@@ -0,0 +1,35 @@
+//! Computes and displays helpful information about the state of the world.
+//!
+//! UI elements generated for / by this work belong in the `ui` module instead.
+
+use bevy::prelude::*;
+use core::fmt::Display;
+
+use crate::asset_management::manifest::{Id, Unit};
+
+/// Systems and reources for communicating the state of the world to the player.
+pub struct InfoVisPlugin;
+
+impl Plugin for InfoVisPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(census).init_resource::<Census>();
+    }
+}
+
+/// Tracks the population of organisms
+#[derive(Debug, Resource, Default)]
+pub(crate) struct Census {
+    /// The total number of units of any kind
+    total_units: usize,
+}
+
+impl Display for Census {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Population: {}", self.total_units)
+    }
+}
+
+/// Counts the number of organisms
+fn census(mut census: ResMut<Census>, unit_query: Query<(), With<Id<Unit>>>) {
+    census.total_units = unit_query.iter().len();
+}
