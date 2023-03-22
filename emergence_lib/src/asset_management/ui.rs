372bc7110ed4ceb5ecf289026d207a157aa9f638
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-22 09:52:41 -0400

    Get list of structure names from manifest

--- emergence_lib/src/asset_management/ui.rs
+++ emergence_lib/src/asset_management/ui.rs
@@ -7,7 +7,7 @@ use core::hash::Hash;
 use crate::player_interaction::terraform::TerraformingChoice;
 
 use super::{
-    manifest::{Id, Structure},
+    manifest::{Id, Structure, StructureManifest},
     Loadable,
 };
 
@@ -50,10 +50,10 @@ impl<D: Send + Sync + 'static + Hash + Eq> Icons<D> {
 impl FromWorld for Icons<Id<Structure>> {
     fn from_world(world: &mut World) -> Self {
         let asset_server = world.resource::<AssetServer>();
-        let mut map = HashMap::new();
+        let structure_manifest = world.resource::<StructureManifest>();
+        let structure_names = structure_manifest.names();
 
-        // TODO: discover this from the file directory
-        let structure_names = vec!["acacia", "leuco", "ant_hive", "hatchery", "storage"];
+        let mut map = HashMap::new();
 
         for id in structure_names {
             let structure_id = Id::from_name(id);
