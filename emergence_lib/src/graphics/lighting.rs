eeadc2e5356823ce98e2c89a056f6bc3ba341a5a
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-23 11:07:52 -0400

    Make the moon period longer for more interest

--- emergence_lib/src/graphics/lighting.rs
+++ emergence_lib/src/graphics/lighting.rs
@@ -87,7 +87,7 @@ impl CelestialBody {
             declination: 23.5 / 360. * PI / 2.,
             travel_axis: PI / 6.,
             illuminance: 1e4,
-            days_per_cycle: 2.0,
+            days_per_cycle: 29.53,
         }
     }
 }
