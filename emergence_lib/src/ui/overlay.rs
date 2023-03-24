4db7aa83b09b65dfbd4fb5ea74e12947cd1690d6
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-24 14:18:33 -0400

    Display currently visualized signal

--- emergence_lib/src/ui/overlay.rs
+++ emergence_lib/src/ui/overlay.rs
@@ -1,18 +1,14 @@
 //! Controls what is being visualized on the terrain by the [`TileOverlay`].
 
-use bevy::prelude::*;
-use bevy::utils::HashMap;
-use derive_more::Display;
-use emergence_macros::IterableEnum;
-
-use crate as emergence_lib;
-
-use crate::enum_iter::IterableEnum;
 use crate::{
-    asset_management::manifest::{Id, ItemManifest, StructureManifest, UnitManifest},
+    asset_management::{
+        manifest::{Id, ItemManifest, StructureManifest, UnitManifest},
+        AssetState,
+    },
     infovis::TileOverlay,
     signals::SignalType,
 };
+use bevy::prelude::*;
 
 use super::{FiraSansFontFamily, LeftPanel};
 
@@ -21,19 +17,16 @@ pub(super) struct OverlayMenuPlugin;
 impl Plugin for OverlayMenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_system(select_overlay)
-            .add_startup_system(setup_overlay_menu);
+            .add_startup_system(setup_overlay_menu)
+            .add_system(update_signal_type_display.run_if(in_state(AssetState::Ready)));
     }
 }
 
 /// Records the structure of the overlay menu. for easy lookup.
 #[derive(Resource)]
 struct OverlayMenu {
-    /// The entity that contains the legend.
-    legend_entity: Entity,
-    /// The panel that contains all of the buttons.
-    button_panel: Entity,
-    /// The buttons that store each [`SignalKind`].
-    signal_kind_buttons: HashMap<SignalKind, Entity>,
+    /// The entity that tracks the [`SignalType`] being displayed.
+    signal_type_entity: Entity,
 }
 
 /// Controls the overlay that is currently being displayed based on UI interactions.
@@ -47,32 +40,6 @@ fn select_overlay(
     }
 }
 
-/// The data-less equivalent of a [`SignalType`].
-#[derive(Component, Display, IterableEnum, Clone, Copy, PartialEq, Eq, Hash, Debug)]
-enum SignalKind {
-    Push,
-    Pull,
-    Stores,
-    Contains,
-    Work,
-    Demolish,
-    Unit,
-}
-
-impl From<SignalType> for SignalKind {
-    fn from(signal_type: SignalType) -> Self {
-        match signal_type {
-            SignalType::Push(_) => Self::Push,
-            SignalType::Pull(_) => Self::Pull,
-            SignalType::Stores(_) => Self::Stores,
-            SignalType::Contains(_) => Self::Contains,
-            SignalType::Work(_) => Self::Work,
-            SignalType::Demolish(_) => Self::Demolish,
-            SignalType::Unit(_) => Self::Unit,
-        }
-    }
-}
-
 /// Creates the UI needed to display the overlay.
 fn setup_overlay_menu(
     mut commands: Commands,
@@ -81,6 +48,22 @@ fn setup_overlay_menu(
     fonts: Res<FiraSansFontFamily>,
 ) {
     let left_panel_entity = left_panel_query.single();
+    let text_style = TextStyle {
+        font: fonts.regular.clone_weak(),
+        font_size: 20.0,
+        color: Color::WHITE,
+    };
+
+    let signal_type_entity = commands
+        .spawn(TextBundle {
+            text: Text::from_section("SIGNAL_TYPE".to_string(), text_style),
+            ..Default::default()
+        })
+        .id();
+    commands
+        .entity(left_panel_entity)
+        .add_child(signal_type_entity);
+
     let legend_entity = commands
         .spawn(ImageBundle {
             style: Style {
@@ -99,82 +82,24 @@ fn setup_overlay_menu(
         .id();
     commands.entity(left_panel_entity).add_child(legend_entity);
 
-    let button_panel = commands
-        .spawn(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Px(100.0), Val::Percent(100.0)),
-                ..Default::default()
-            },
-            background_color: BackgroundColor(Color::PINK),
-            ..Default::default()
-        })
-        .id();
-    commands.entity(left_panel_entity).add_child(button_panel);
+    commands.insert_resource(OverlayMenu { signal_type_entity });
+}
 
-    let signal_kind_column = commands
-        .spawn(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Px(100.0), Val::Px(50.0)),
-                flex_direction: FlexDirection::Column,
-                ..Default::default()
-            },
-            background_color: BackgroundColor(Color::RED),
-            ..Default::default()
-        })
-        .id();
-    let signal_id_column = commands
-        .spawn(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Px(100.0), Val::Px(50.0)),
-                flex_direction: FlexDirection::Column,
-                ..Default::default()
-            },
-            background_color: BackgroundColor(Color::BLUE),
-            ..Default::default()
-        })
-        .id();
-    commands
-        .entity(button_panel)
-        .push_children(&[signal_kind_column, signal_id_column]);
+fn update_signal_type_display(
+    mut query: Query<&mut Text>,
+    overlay_menu: Res<OverlayMenu>,
+    tile_overlay: Res<TileOverlay>,
+    item_manifest: Res<ItemManifest>,
+    structure_manifest: Res<StructureManifest>,
+    unit_manifest: Res<UnitManifest>,
+) {
+    let mut text = query.get_mut(overlay_menu.signal_type_entity).unwrap();
 
-    let text_style = TextStyle {
-        font: fonts.regular.clone_weak(),
-        font_size: 20.0,
-        color: Color::BLACK,
+    text.sections[0].value = match tile_overlay.visualized_signal {
+        Some(signal_type) => format!(
+            "{}",
+            signal_type.display(&item_manifest, &structure_manifest, &unit_manifest)
+        ),
+        None => "No signal".to_string(),
     };
-
-    let mut signal_kind_buttons = HashMap::new();
-    for signal_kind in SignalKind::variants() {
-        let signal_kind_button_entity = commands
-            .spawn(ButtonBundle {
-                style: Style {
-                    size: Size::new(Val::Px(100.0), Val::Px(50.0)),
-                    // Vertically center the text in the button.
-                    align_items: AlignItems::Center,
-                    ..Default::default()
-                },
-                background_color: BackgroundColor(Color::GREEN),
-                ..Default::default()
-            })
-            .insert(signal_kind)
-            .with_children(|parent| {
-                parent.spawn(TextBundle {
-                    text: Text::from_section(format!("{signal_kind}"), text_style.clone()),
-                    ..Default::default()
-                });
-            })
-            .id();
-
-        commands
-            .entity(button_panel)
-            .add_child(signal_kind_button_entity);
-
-        signal_kind_buttons.insert(signal_kind, signal_kind_button_entity);
-    }
-
-    commands.insert_resource(OverlayMenu {
-        legend_entity,
-        button_panel,
-        signal_kind_buttons,
-    });
 }
