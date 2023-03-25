ba85adefd849d5c7784fd281701af53ba4689af9
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-25 16:41:06 -0400

    Add bevy_framepace

--- emergence_game/src/main.rs
+++ emergence_game/src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy::window::{PresentMode, WindowPlugin};
+use bevy_framepace::{FramepacePlugin, FramepaceSettings, Limiter};
 use emergence_lib::simulation::generation::GenerationConfig;
 
 fn main() {
@@ -12,6 +13,10 @@ fn main() {
             }),
             ..Default::default()
         }))
+        .add_plugin(FramepacePlugin)
+        .insert_resource(FramepaceSettings {
+            limiter: Limiter::Auto,
+        })
         .add_plugin(emergence_lib::simulation::GeometryPlugin {
             gen_config: GenerationConfig::default(),
         })
