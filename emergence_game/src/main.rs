101eb83c754981e06591d5c1596107dc285172ea
Alice Cecile <alice.i.cecile@gmail.com>
2023-03-30 17:31:41 -0400

    Swap to borderless full screen as default mode (#632)

--- emergence_game/src/main.rs
+++ emergence_game/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
             primary_window: Some(Window {
                 title: "Emergence".to_string(),
                 present_mode: PresentMode::AutoNoVsync,
-                mode: WindowMode::Fullscreen,
+                mode: WindowMode::BorderlessFullscreen,
                 ..default()
             }),
             ..Default::default()
