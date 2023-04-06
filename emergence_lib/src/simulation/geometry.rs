0f298e02841f81b6a3fd316f809f6b2cf7faccea
Alice Cecile <alice.i.cecile@gmail.com>
2023-04-06 18:18:52 -0400

    Fix bug in get_ghost_terrain

--- emergence_lib/src/simulation/geometry.rs
+++ emergence_lib/src/simulation/geometry.rs
@@ -542,7 +542,7 @@ impl MapGeometry {
 
     /// Gets the ghost terrain [`Entity`] at the provided `tile_pos`, if any.
     pub(crate) fn get_ghost_terrain(&self, tile_pos: TilePos) -> Option<Entity> {
-        self.ghost_structure_index.get(&tile_pos).copied()
+        self.ghost_terrain_index.get(&tile_pos).copied()
     }
 }
 
