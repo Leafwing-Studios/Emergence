bef8c918553df434f256647e6fd962a760ba65de
Alice Cecile <alice.i.cecile@gmail.com>
2023-04-08 22:07:30 -0400

    Cache and use previous inventory state

--- emergence_lib/src/graphics/terrain.rs
+++ emergence_lib/src/graphics/terrain.rs
@@ -13,34 +13,45 @@ use bevy::{prelude::*, utils::HashMap};
 pub(super) fn manage_litter_piles(
     terrain_handles: Res<TerrainHandles>,
     // A simple cache of the current litter piles.
-    mut current_litter_piles: Local<HashMap<TilePos, Entity>>,
+    mut current_litter_piles: Local<HashMap<TilePos, (InventoryState, Entity)>>,
     terrain_query: Query<(&TilePos, Ref<StorageInventory>), With<Id<Terrain>>>,
     mut commands: Commands,
     map_geometry: Res<MapGeometry>,
 ) {
     for (&tile_pos, storage_inventory) in terrain_query.iter() {
-        if storage_inventory.is_changed() {
-            // Clean up any old models
-            if let Some(&entity) = current_litter_piles.get(&tile_pos) {
-                commands.entity(entity).despawn_recursive();
-                current_litter_piles.remove(&tile_pos);
-            }
+        if !storage_inventory.is_changed() {
+            continue;
+        }
 
-            let inventory_state = storage_inventory.state();
-            // Don't spawn anything if there's no litter.
-            if inventory_state != InventoryState::Empty {
-                let scene_handle = terrain_handles.litter_models.get(&inventory_state).unwrap();
-                let litter_entity = commands
-                    .spawn(SceneBundle {
-                        scene: scene_handle.clone(),
-                        transform: Transform::from_translation(
-                            tile_pos.into_world_pos(&map_geometry),
-                        ),
-                        ..Default::default()
-                    })
-                    .id();
-                current_litter_piles.insert(tile_pos, litter_entity);
+        let current_inventory_state = storage_inventory.state();
+
+        // Clean up any old models
+        if let Some((previous_inventory_state, entity)) = current_litter_piles.get(&tile_pos) {
+            // Only despawn if the inventory state has changed.
+            if *previous_inventory_state != current_inventory_state {
+                commands.entity(*entity).despawn_recursive();
+                current_litter_piles.remove(&tile_pos);
+            } else {
+                continue;
             }
         }
+
+        // Don't spawn anything if there's no litter.
+        if current_inventory_state == InventoryState::Empty {
+            continue;
+        }
+
+        let scene_handle = terrain_handles
+            .litter_models
+            .get(&current_inventory_state)
+            .unwrap();
+        let litter_entity = commands
+            .spawn(SceneBundle {
+                scene: scene_handle.clone(),
+                transform: Transform::from_translation(tile_pos.into_world_pos(&map_geometry)),
+                ..Default::default()
+            })
+            .id();
+        current_litter_piles.insert(tile_pos, (current_inventory_state, litter_entity));
     }
 }
