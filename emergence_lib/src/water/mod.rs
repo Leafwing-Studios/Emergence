168e6809df847df910a323b208fa4b5fe4ce28ac
Alice Cecile <alice.i.cecile@gmail.com>
2023-05-04 16:16:58 -0400

    Reproduce fractal patterns

--- emergence_lib/src/water/mod.rs
+++ emergence_lib/src/water/mod.rs
@@ -76,9 +76,9 @@ impl WaterConfig {
         emission_rate: Volume(1e4),
         emission_pressure: Height(1.0),
         water_items_per_tile: 50.0,
-        relative_soil_water_capacity: 0.0,
+        relative_soil_water_capacity: 0.02,
         lateral_flow_rate: 1e3,
-        soil_lateral_flow_ratio: 0.0,
+        soil_lateral_flow_ratio: 0.1,
         enable_oceans: true,
         tide_settings: TideSettings {
             amplitude: Height(3.0),
