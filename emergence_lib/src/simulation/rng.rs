e2c2584b3d0f77275e28872b9f6594cb35b1da52
Alice Cecile <alice.i.cecile@gmail.com>
2023-06-16 12:55:09 -0400

    Missing docs

--- emergence_lib/src/simulation/rng.rs
+++ emergence_lib/src/simulation/rng.rs
@@ -11,10 +11,12 @@ use rand::{rngs::SmallRng, SeedableRng};
 pub(crate) struct GlobalRng(SmallRng);
 
 impl GlobalRng {
+    /// Creates a new seeded RNG
     pub(crate) fn new(seed: u64) -> Self {
         Self(SmallRng::seed_from_u64(seed))
     }
 
+    /// Provides access to the underlying RNG so that methods can be called using it.
     pub(crate) fn get_mut(&mut self) -> &mut SmallRng {
         &mut self.0
     }
