3c9a370ff1b0fc16ec9dfcc83431b422cb773604
Alice Cecile <alice.i.cecile@gmail.com>
2023-11-21 15:03:44 -0500

    try_remove unused

--- emergence_lib/src/utils/fallible_commands.rs
+++ emergence_lib/src/utils/fallible_commands.rs
@@ -6,13 +6,6 @@ use bevy::{ecs::system::EntityCommands, prelude::*};
 /// the most commonly used commands.
 
 pub trait FallibleEntityCommandExt<'w, 's, 'a> {
-    /// Attempts to remove a component or bundle from the entity.
-    ///
-    /// Fails silently (rather than panicking) if the entity does not exist.
-    ///
-    /// Fallible version of [`EntityCommands::remove`].
-    fn try_remove<B: Bundle>(&mut self) -> &mut EntityCommands<'w, 's, 'a>;
-
     /// Attempts to add a child entity to the entity.
     ///
     /// Fails silently (rather than panicking) if the entity does not exist.
@@ -22,15 +15,6 @@ pub trait FallibleEntityCommandExt<'w, 's, 'a> {
 }
 
 impl<'w, 's, 'a> FallibleEntityCommandExt<'w, 's, 'a> for EntityCommands<'w, 's, 'a> {
-    fn try_remove<B: Bundle>(&mut self) -> &mut Self {
-        self.add(|entity, world: &mut World| {
-            if let Some(mut entity_mut) = world.get_entity_mut(entity) {
-                entity_mut.remove::<B>();
-            }
-        });
-        self
-    }
-
     fn try_add_child(&mut self, child: Entity) -> &mut EntityCommands<'w, 's, 'a> {
         self.add(move |entity, world: &mut World| {
             if let Some(mut entity_mut) = world.get_entity_mut(entity) {
